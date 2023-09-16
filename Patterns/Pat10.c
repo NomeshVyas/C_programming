710d00c549c26df56d7f5c8b8ee90331a3549911
NomeshVyas <nomesh9351962867@gmail.com>
2023-09-16 23:57:37 +0530

    Pat10 and Pat11

---
+++ Patterns/Pat10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //// Pattern 10 - Inverted full pyramid of *
+    // * * * * * * * * *
+    //   * * * * * * *
+    //     * * * * *
+    //       * * *
+    //         *
+    
+    int n, i, j, space=0;
+    printf("Enter the rows you want: ");
+    scanf("%d", &n);
+    printf("\n");
+
+    for(i=n; i>=1; i--){
+        for(space=0; space<n-i; space++)
+            printf("   ");
+
+        for(j=0; j<i*2-1; j++)
+            printf(" * ");
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Patterns/Pat11.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //// Pattern 11 - Floyd's Triangle.
+    // 1
+    // 2 3
+    // 4 5 6
+    // 7 8 9 10
+    
+    int n, i, j, k=1;
+    printf("Enter the rows you want: ");
+    scanf("%d", &n);
+    printf("\n");
+
+    for(i=0; i<n; i++){
+        for(j=0; j<=i; j++){
+            printf(" %d ", k);
+            k+=1;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
