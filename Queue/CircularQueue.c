58367839e8e6d7a5cf166750a41a06cd4711de37
NomeshVyas <nomesh9351962867@gmail.com>
2023-10-24 00:13:04 +0530

    linear and circular queue

---
+++ Queue/CircularQueue.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+struct queue{
+    int front, rear, size, capacity;
+    int * arr;
+};
+
+struct queue* createQueue(int capacity){
+    struct queue* queue = (struct queue*)malloc(sizeof(struct queue));
+    queue->capacity = capacity;
+    queue->front = queue->size = 0;
+    queue->rear = capacity-1;
+    queue->arr = (int*)malloc(queue->capacity * sizeof(int));
+    return queue;
+}
+
+int isFull(struct queue* queue){
+    return (queue->size == queue->capacity);
+}
+
+int isEmpty(struct queue* queue){
+    return (queue->size == 0);
+}
+
+void enqueue (struct queue* queue, int item){
+    if(isFull(queue))
+        return;
+    queue->rear = (queue->rear+1)%queue->capacity;
+    queue->arr[queue->rear] = item;
+    queue->size++;
+}
+
+int dequeue (struct queue* queue){
+    if(isEmpty(queue))
+        return INT_MIN;
+    int item = queue->arr[queue->front];
+    queue->front = (queue->front+1)%queue->capacity;
+    queue->size--;
+    return item;
+}
+
+int front (struct queue* queue){
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->arr[queue->front];    
+}
+
+int rear (struct queue* queue){
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->arr[queue->rear];
+}
+
+void printQueue(struct queue* queue){
+    for(int i = queue->front; i <= queue->rear; i++)
+        printf("%d\t", queue->arr[i]);
+    printf("\n");
+}
+
+int main(){
+    struct queue* q = createQueue(5);
+
+    enqueue(q, 10);
+    enqueue(q, 20);
+    enqueue(q, 30);
+    enqueue(q, 40);
+    enqueue(q, 50);
+
+    printQueue(q);
+    dequeue(q);
+    printQueue(q);
+
+    printf("\nFront: %d", front(q));
+    printf("\nRear: %d", rear(q));
+    return 0;
+}
---
+++ Queue/SimpleQueue.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+struct queue{
+    int size, front, rear;
+    int * arr;
+};
+
+struct queue * createQueue(int size){
+    struct queue* queue = (struct queue*)malloc(sizeof(struct queue));
+    queue->size = size;
+    queue->front = queue->rear = -1;
+    queue->arr = (int *)malloc(queue->size * sizeof(int));
+    return queue;
+}
+
+int isEmpty(struct queue* queue){
+    return (queue->rear == -1);
+}
+
+int isFull(struct queue* queue){
+    return (queue->rear == queue->size-1);
+}
+
+void enqueue(struct queue* queue, int item){
+    if(isFull(queue))
+        return;
+    else if(queue->front == -1 && queue->rear == -1)
+        queue->front = 0;
+    queue->arr[++queue->rear] = item;
+}
+
+int dequeue(struct queue* queue){
+    if(isEmpty(queue))
+        return INT_MIN;
+    return queue->arr[queue->front++];
+
+}
+
+int front(struct queue* queue){
+    if(isEmpty(queue))
+        return INT_MIN;
+    return queue->arr[queue->front];
+}
+
+int rear(struct queue* queue){
+    if(isEmpty(queue))
+        return INT_MIN;
+    return queue->arr[queue->rear];
+}
+
+void printQueue(struct queue* queue){
+    for(int i=queue->front; i <= queue->rear; i++)
+        printf("%d\t", queue->arr[i]);
+    printf("\n");
+}
+
+int main(){
+    struct queue* q = createQueue(10);
+
+    enqueue(q, 10);
+    enqueue(q, 20);
+    enqueue(q, 30);
+    enqueue(q, 40);
+    enqueue(q, 50);
+
+    printQueue(q);
+    
+    dequeue(q);
+    printQueue(q);
+    
+    printf("\nFront: %d", front(q));
+    printf("\nRear: %d", rear(q));
+    return 0;
+}
